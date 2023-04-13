22bef295d49894338dc7631facabee3b8659dcb0
Katerina-Kor <katerina.korobeiko@gmail.com>
2023-04-13 18:00:43 +0400

    feat: add solution for vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type) {
+    this.type = type;
+    console.debug(this.type);
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let res = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyIterator = 0;
+    let messageLength = message.length;
+    let keyLength = key.length;
+    let alphabetLength = alphabet.length;
+
+    for (let i = 0; i < messageLength; i++) {
+      let messageIndex = alphabet.indexOf(message[i]);
+      let keyIndex = alphabet.indexOf((keyIterator >= keyLength) ? key[keyIterator % keyLength] : key[keyIterator]);
+      if (messageIndex == -1) {
+        res += message[i];
+        continue;
+      }
+      let letterIndex = messageIndex + keyIndex;
+      if (letterIndex >= alphabetLength) letterIndex = letterIndex % alphabetLength;
+      res += alphabet[letterIndex];
+      keyIterator++;
+    }
+    if (this.type === false) {
+      res = Array.from(res).reverse().join('');
+    }
+    return res;
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let res = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyIterator = 0;
+    let messageLength = message.length;
+    let keyLength = key.length;
+    let alphabetLength = alphabet.length;
+
+    for (let i = 0; i < messageLength; i++) {
+      let messageIndex = alphabet.indexOf(message[i]);
+      let keyIndex = alphabet.indexOf((keyIterator >= keyLength) ? key[keyIterator % keyLength] : key[keyIterator]);
+      if (messageIndex == -1) {
+        res += message[i];
+        continue;
+      }
+      let letterIndex = messageIndex - keyIndex;
+      if (letterIndex < 0) letterIndex = letterIndex + alphabetLength;
+      res += alphabet[letterIndex];
+      keyIterator++;
+    }
+    
+    if (this.type === false) {
+      res = Array.from(res).reverse().join('');
+    }
+    return res;
   }
 }
 
